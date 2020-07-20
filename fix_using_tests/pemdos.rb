6107b7c2f828488ca42954d960269f7316c2dfa9
Jhonny Chamoun <jhonny.chamoun@gmail.com>
2020-07-20 15:55:57 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,12 @@
 # don't forget to add: require 'pry'
-
+require 'pry'
 def snake_it_up(string)
+  #binding.pry
   if string[0] == "s"
-    10 * "s" + string
+    10.times { string = "s" + string}
+    #binding.pry
   else
     string
   end
+  string
 end
